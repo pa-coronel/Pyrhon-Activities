7b1b21b797ca1ca3360d3c174e6f369efdd92547
pa-coronel <131678469+pa-coronel@users.noreply.github.com>
2023-04-24 21:30:53 +0800

    Create camelcase.py

---
+++ camelcase.py
@@ -0,0 +1,12 @@
+def camelcase():
+    word = input("enter a word: ")
+    newword = ''
+    for i in word:
+        if i.isupper():
+            newword += " "
+        newword += i
+
+    print("the word:", newword)
+    
+#run function
+camelcase()
